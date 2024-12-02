30bd5df614da6e81bbfd1e41ba3a857f99c60b08
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2024-12-02 19:11:52 +0530

    collaborative wirting

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+export const Editor = () => {
+  const editor = useEditor({
+    editorProps: {
+        attributes: {
+            style: "padding-left: 56px; padding-right: 56px;",
+            class: "focus:outline-none print:border-0 border bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+        }
+    },
+    extensions: [
+        StarterKit,
+        TaskItem.configure({
+            nested: true,
+        }),
+        TaskList,
+    ],
+    content: "<p>Hello World! <p>",
+  });
+
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:overflow-visible print:bg-white">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor}/>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,16 @@
+import { Editor } from "./editor";
+
+// interface DocumentIdPageProps {
+//   params: Promise<{ documentId: string }>;
+// }
+
+const DocumentIdPage = async () => {
+
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentIdPage;
